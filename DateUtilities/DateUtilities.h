4c4b8925a012aa5243d8d84004322a592bcd10bf
Jonathan Wong <jonwong@outlook.com>
2019-02-09 13:30:50 -0800

    Initial Commit

---
+++ DateUtilities/DateUtilities.h
@@ -0,0 +1,19 @@
+//
+//  DateUtilities.h
+//  DateUtilities
+//
+//  Created by Jonathan Wong on 2/9/19.
+//  Copyright © 2019 Jonathan Wong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DateUtilities.
+FOUNDATION_EXPORT double DateUtilitiesVersionNumber;
+
+//! Project version string for DateUtilities.
+FOUNDATION_EXPORT const unsigned char DateUtilitiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DateUtilities/PublicHeader.h>
+
+
